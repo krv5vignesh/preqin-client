6391ce677a96dac46c209afc8aefeb5b3c1d827c
Vignesh Selvan K R <krv5vignesh@gmail.com>
2024-07-25 17:00:56 +0530

    Added nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="nav">
+          <Link href="/">Home</Link>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
