de479325d95e939db14c92bd8fca4191d18df8fd
Vignesh Selvan K R <krv5vignesh@gmail.com>
2024-07-26 00:40:02 +0530

    Added cypress tests

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,19 @@
+describe('Navigation', () => {
+    it('grid should be available in the homepage', () => {
+      cy.visit('http://localhost:3001/')
+      cy.get('div.MuiDataGrid-root').should('exist');
+    })
+
+    it('Clicking on a row should navigate to investor details page', () => {
+        cy.visit('http://localhost:3001/')
+        cy.get('div.MuiDataGrid-row').first().click();
+        cy.url().should('include', '/investor/');
+    })
+
+    it('Selecting a asset class should load the grid', () => {
+        cy.visit('http://localhost:3001/investor/2670');
+        cy.get('#asset-class-select').click();
+        cy.get('li.MuiMenuItem-root').contains('pe').click();
+        cy.get('div.MuiDataGrid-root').should('exist');
+      })
+  })
