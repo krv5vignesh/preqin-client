eba078f2e77626996e933d4ec94916c109ac7d37
Vignesh Selvan K R <krv5vignesh@gmail.com>
2024-07-25 15:53:10 +0530

    Added investors grid to the homepage

---
+++ app/components/InvestorsGrid.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import styles from "../page.module.css";
+import { useRouter } from 'next/navigation';
+
+export const InvestorsGrid = ({data} : {data: Array<Investors>}) => {
+  const router = useRouter();
+
+  const rows = data.map(item => ({
+    id: item.firm_id,
+    firm_id: item.firm_id,
+    firm_name: item.firm_name,
+    firm_type: item.firm_type,
+    date_added: new Date(item.date_added)?.toLocaleDateString(),
+    address: item.address 
+  }));
+
+  const columns: GridColDef[] = [
+    { field: 'firm_id', headerName: 'Firm ID', width: 100 },
+    { field: 'firm_name', headerName: 'Firm name', width: 150 },
+    { field: 'firm_type', headerName: 'Type', width: 150 },
+    { field: 'date_added', headerName: 'Date added', width: 150 },
+    { field: 'address', headerName: 'Address', width: 250 },
+  ];
+
+    return <DataGrid 
+        className={styles.investorsGrid} 
+        rows={rows} 
+        columns={columns}
+        onRowClick={(params) => router.push(`/investor/${params.id}`)} 
+    />
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
+import { InvestorsGrid } from "./components/InvestorsGrid";
 import styles from "./page.module.css";
 import React from "react";
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 const getInvestors = async () => {
   const url = "http://localhost:8000/api/investors";
@@ -15,29 +15,14 @@ const getInvestors = async () => {
 export default async function Home() {
   const investorData:Array<Investors> = await getInvestors();
 
-  const rows = investorData.map(item => ({
-    id: item.firm_id,
-    firm_id: item.firm_id,
-    firm_name: item.firm_name,
-    data_added: item.date_added,
-    address: item.address 
-  }));
-
-  const columns: GridColDef[] = [
-    { field: 'firm_id', headerName: 'Firm ID', width: 150 },
-    { field: 'firm_name', headerName: 'Firm name', width: 150 },
-    { field: 'firm_type', headerName: 'Type', width: 150 },
-    { field: 'date_added', headerName: 'Date added', width: 150 },
-    { field: 'address', headerName: 'Address', width: 150 },
-  ];
-
-
 
   return (
     <main className={styles.main}>
       <div className={styles.center}>
+        <div className={styles.title}> Preqin Investors</div>
         {
-          investorData && <DataGrid className={styles.investorsGrid} rows={rows} columns={columns} />
+          investorData && 
+            <InvestorsGrid data={investorData} />
         }
       </div>
     </main>
